07948ecd4c6534a75846ec13328dfdb4cb264cb1
Gökhan Demirkiyik <gdem@java-developer.ch>
2023-04-15 00:47:32 +0200

    Removed patched code because jackson-datatype-jakarta-jsonp supports JSONP implementation based on new (jakarta) json-P API

--- app/src/main/java/ch/softwareplus/blueprints/config/JacksonConfig.java
+++ app/src/main/java/ch/softwareplus/blueprints/config/JacksonConfig.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr353.JSR353Module;
+import com.fasterxml.jackson.datatype.jsonp.JSONPModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,7 +20,7 @@ public class JacksonConfig {
                 .setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL)
                 .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        mapper.registerModule(new JSR353Module());
+        mapper.registerModule(new JSONPModule());
         return mapper;
     }
 }
